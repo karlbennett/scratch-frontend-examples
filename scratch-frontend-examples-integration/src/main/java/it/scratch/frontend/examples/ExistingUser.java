f59a06c5d9d24a5a9b1d927599299a06d911f114
Karl Bennett <karl.bennett.smt@gmail.com>
2017-04-23 14:34:15 +1200

    Removed the unnecessary extra User class.

--- scratch-frontend-examples-integration/src/main/java/it/scratch/frontend/examples/ExistingUser.java
+++ scratch-frontend-examples-integration/src/main/java/it/scratch/frontend/examples/ExistingUser.java
@@ -2,6 +2,7 @@ package it.scratch.frontend.examples;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import scratch.frontend.examples.domain.User;
 
 @Component
 @ConfigurationProperties("user.existing")
